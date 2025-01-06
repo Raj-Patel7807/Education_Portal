88cf808ce6e7313d64598788b7890ba63d0dc7f8
Raj Patel <rajpatel7807@gmail.com>
2025-01-06 18:44:42 +0530

    Update Index

---
+++ script.js
@@ -0,0 +1,9 @@
+const logout = document.querySelector(".navbar .logout");
+
+logout.addEventListener("click", () => {
+    window.location.href = "Login/login.html";
+});
+
+document.querySelector(".thumbnail").addEventListener("click", () => {
+    window.location.href = "index.html";
+});
